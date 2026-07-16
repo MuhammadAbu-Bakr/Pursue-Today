0ee079f9b1425c88cdbe7b22f6254baeab56b074
Muhammad Abu-Bakr <abubakrmuhammed075@gmail.com>
2026-07-16 01:55:04 -0700

    Fixes

--- todo-app/client/src/context/auth-context.jsx
+++ todo-app/client/src/context/auth-context.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useEffect, useState } from "react";
 
-const API_BASE = "https://pursue-today-api.onrender.com/api"; 
-// "https://pursue-today-api.onrender.com/api"
+const API_BASE = "https://pursue-today.onrender.com/api"; 
+// "https://pursue-today.onrender.com/api"
 // "http://localhost:5000/api"
 
 async function request(url, options = {}) {
